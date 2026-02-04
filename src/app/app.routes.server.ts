5d56170b066a6d5c4ede913bfc4e0806259e2a32
Your Name <you@example.com>
2026-02-04 21:54:42 +0100

    sec commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,11 @@
+// src/app/app.routes.server.ts
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+
+  // ✅ dynamic -> do NOT prerender
+  { path: 'voitures/:id', renderMode: RenderMode.Server }, // or RenderMode.Client
+
+  { path: '**', renderMode: RenderMode.Server },
 ];
