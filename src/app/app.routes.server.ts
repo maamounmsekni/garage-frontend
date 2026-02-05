079f374cd5aa7a8c3fa5ac0f09715721a1ce464f
Your Name <you@example.com>
2026-02-05 01:10:43 +0100

    secend co

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,5 +3,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   { path: '', renderMode: RenderMode.Prerender },
   { path: 'historique', renderMode: RenderMode.Server },
-  { path: '**', renderMode: RenderMode.Server },
+  { path: 'voitures/:id', renderMode: RenderMode.Server },
+  { path: '**', renderMode: RenderMode.Server },  // Catch all route, ensuring SSR for all paths
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,13 @@
 import { Routes } from '@angular/router';
 
 import { HistoriquePage } from './features/historique/historique.page';
-import { ProprietairesPage } from './features/proprietaires/proprietaires.page';
-import { TypesVoiturePage } from './features/types-voiture/types-voiture.page';
 import { VoituresPage } from './features/voitures/voitures.page';
 import { VoitureDetailsPage } from './features/voiture-details/voiture-details.page';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'historique', pathMatch: 'full' },
-
   { path: 'historique', component: HistoriquePage },
   { path: 'voitures', component: VoituresPage },
   { path: 'voitures/:id', component: VoitureDetailsPage },
-  { path: 'proprietaires', component: ProprietairesPage },
-  { path: 'types-voiture', component: TypesVoiturePage },
-
-  { path: '**', redirectTo: 'historique' },
+  { path: '**', redirectTo: 'historique' }, // This ensures any unknown routes will redirect
 ];
