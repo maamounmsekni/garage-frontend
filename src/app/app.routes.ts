d5e2089bff867a6d37aaf2283b265b7e4a0d7eed
Your Name <you@example.com>
2026-02-08 20:11:01 +0100

    without /historique

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { VoituresComponent } from './features/voitures/voitures.component';
 import { VoitureDetailsComponent } from './features/voiture-details/voiture-details.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'historique', pathMatch: 'full' },
+  { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'historique', component: HistoriquePage },
   { path: 'voitures', component: VoituresComponent },
   { path: 'voitures/:id', component: VoitureDetailsComponent },
